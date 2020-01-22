9c4f30efc8186e3bbcb46c82bc5362002005e8f2
oceenachi <phillipfrancisca@gmail.com>
2020-01-22 11:11:16 +0100

    add routing for both views

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Stat from "../src/component/stat/Stat";
+import Verification from "./verification";
+import StatState from '../src/component/context/card/StatState';
+
+function App() {
+  return (
+    <>
+      <h1>Vela-Verify</h1>
+      <Router>
+      <StatState>
+        <Switch>
+          <Route exact path="/">
+            <Verification />
+          </Route>
+          <Route exact path="/verify" component={Verification}></Route>
+          <Route exact path="/stats" component={Stat}></Route>
+         
+        </Switch>
+        </StatState>
+      </Router>
+    </>
+  );
+}
+
+export default App;
